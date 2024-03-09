e79d32a1ecc671d48b28a89b78ed70f69de0df27
“crlnvls” <cveloso0808@gmail.com>
2024-03-09 12:08:58 +0000

    Create footer structure

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,7 +3,28 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <>
-      <h1>Footer</h1>
+      <div>
+        <div>
+          <h3>make money by controlling YOUR money</h3>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <a href="">Careers</a>
+            </li>
+            <li>
+              <a href="">Contact us</a>
+            </li>
+            <li>
+              <a href="">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="">Terms and Conditions</a>
+            </li>
+          </ul>
+        </div>
+        <div>// Social Media Icons</div>
+      </div>
     </>
   );
 };
