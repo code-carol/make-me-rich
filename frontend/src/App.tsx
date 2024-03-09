a31ec22f736d534abfdffec9fef005df0b3ceebb
“crlnvls” <cveloso0808@gmail.com>
2024-03-09 16:34:14 +0000

    Add Login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/login" element={<Login />}></Route>
       <Route path="*" element={<NotFound />}></Route>
     </Routes>
   );
