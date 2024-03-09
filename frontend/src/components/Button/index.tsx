43d59f6d9489f72fac9725b2963d49202c2c60e1
“crlnvls” <cveloso0808@gmail.com>
2024-03-09 11:59:37 +0000

    Create Button component

---
+++ frontend/src/components/Button/index.tsx
@@ -0,0 +1,20 @@
+import "./Button.css";
+
+interface BtnProps {
+  className: string;
+  children: string;
+  handleOnClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  type: "submit" | "reset" | "button";
+}
+
+const Button = ({ className, children, handleOnClick, type }: BtnProps) => {
+  return (
+    <>
+      <button className={className} onClick={handleOnClick} type={type}>
+        {children}
+      </button>
+    </>
+  );
+};
+
+export default Button;
