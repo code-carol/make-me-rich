ec1010ac728e5c95615fc0b3f2df529edf502064
Silvia Neves <siltatiana.sn@gmail.com>
2024-03-21 20:31:54 +0000

    Add goalsRoutes to app.js, goalsControllers

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,12 @@ const app = express();
 const expensesRoutes = require("./routes/expensesRoutes");
 const budgetRoutes = require("./routes/budgetRoutes");
 const categoriesRoutes = require("./routes/categoriesRoutes");
+const goalsRoutes = require("./routes/goalsRoutes");
 
 app.use(express.json());
 app.use("/expenses", expensesRoutes);
 app.use("/budget", budgetRoutes);
 app.use("/category", categoriesRoutes);
+app.use("/goal", goalsRoutes);
 
 module.exports = app;
---
+++ backend/src/controllers/goalsController.js
@@ -0,0 +1,119 @@
+const goalModel = require("../models/goalModel");
+
+const createGoal = (req, res) => {
+  const goal = new goalModel(req.body);
+  goal
+    .save()
+    .then((savedGoal) => {
+      res.status(201).json({
+        message: "Goal created successfully.",
+        CreateGoal: savedGoal,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong. Please try again later.",
+        CreateGoal: error,
+      });
+    });
+};
+
+const getAllGoals = (req, res) => {
+  goalModel.find((error, goals) => {
+    if (goals) {
+      res.status(200).json({
+        message: "All goals fetched successfully.",
+        GetAllGoals: goals,
+      });
+    } else if (goals.length === 0) {
+      res.status(404).json({
+        message: "No goals found.",
+        GetAllGoals: goals,
+      });
+    } else {
+      res.status(500).json({
+        message: "Something went wrong. Please try again later.",
+        GetAllGoals: error,
+      });
+    }
+  });
+};
+
+const getGoalById = (req, res) => {
+  const id = req.params.id;
+  goalModel.findById(id, (error, goal) => {
+    if (goal) {
+      res.status(200).json({
+        message: "Goal fetched successfully.",
+        GetGoalById: goal,
+      });
+    } else if (!goal) {
+      res.status(404).json({
+        message: "Goal not found.",
+        GetGoalById: goal,
+      });
+    } else {
+      res.status(500).json({
+        message: "Something went wrong. Please try again later.",
+        GetGoalById: error,
+      });
+    }
+  });
+};
+
+const updateGoalById = (req, res) => {
+  const id = req.params.id;
+  goalModel.findByIdAndUpdate(
+    id,
+    req.body,
+    { new: true },
+    (error, updatedGoal) => {
+      if (updatedGoal) {
+        res.status(200).json({
+          message: "Goal updated successfully.",
+          UpdateGoal: updatedGoal,
+        });
+      } else if (!updatedGoal) {
+        res.status(404).json({
+          message: "Goal not found.",
+          UpdateGoal: updatedGoal,
+        });
+      } else {
+        res.status(500).json({
+          message: "Something went wrong. Please try again later.",
+          UpdateGoal: error,
+        });
+      }
+    }
+  );
+};
+
+const deleteGoalById = (req, res) => {
+  const id = req.params.id;
+  goalModel.findByIdAndDelete(id, (error, deletedGoal) => {
+    if (deletedGoal) {
+      res.status(200).json({
+        message: "Goal deleted successfully.",
+        DeleteGoal: deletedGoal,
+      });
+    } else if (!deletedGoal) {
+      res.status(404).json({
+        message: "Goal not found.",
+        DeleteGoal: deletedGoal,
+      });
+    } else {
+      res.status(500).json({
+        message: "Something went wrong. Please try again later.",
+        DeleteGoal: error,
+      });
+    }
+  });
+};
+
+module.exports = {
+  createGoal,
+  getAllGoals,
+  getGoalById,
+  updateGoalById,
+  deleteGoalById,
+};
